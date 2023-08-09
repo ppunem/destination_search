54279e0860958553e93ab1ed66c47ae54603da2b
ppunem <prathyusha2094@gmail.com>
2023-08-09 17:56:54 +0000

    undefined destinationList

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,5 +1,5 @@
-import DestinationItem from './components/DestinationItem/index.js'
 import {Component} from 'react'
+import DestinationItem from '../DestinationItem'
 import './index.css'
 
 class DestinationSearch extends Component {
@@ -12,29 +12,31 @@ class DestinationSearch extends Component {
   render() {
     const {searchInput} = this.state
     const {destinationsList} = this.props
-    const searchResults=destinationsList.filter(each=>{
-        each.name.toLowerCase().includes(searchInput.toLowerCase())
-    })
+    const searchResults = destinationsList.filter(each =>
+      each.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
     return (
-      <div className="main-container">
-        <h1 className="main-heading">Destination Search</h1>
-        <input
-          className="user-input"
-          type="search"
-          placeholder="Search"
-          onChange={this.valueChange}
-          value={searchInput}
-        />
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
-          alt="search icon"
-        />
+      <div>
+        <div className="main-container">
+          <h1 className="main-heading">Destination Search</h1>
+          <input
+            className="user-input"
+            type="search"
+            placeholder="Search"
+            onChange={this.valueChange}
+            value={searchInput}
+          />
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+            alt="search icon"
+          />
+        </div>
+        <ul className="result-container">
+          {searchResults.map(each => (
+            <DestinationItem destinationDetails={each} key={each.id} />
+          ))}
+        </ul>
       </div>
-      <ul className="result-container">
-             {searchResults.map(each=>(
-                 <DestinationItem destinationDetails={each} key={each.id}/>
-             ))}
-      </ul>
     )
   }
 }
